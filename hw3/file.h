0fe9e21d7b709dbe4a7c12cb9e3a76024a23335c
WoodyYo <lturtsamuel@gmail.com>
2014-11-30 02:32:01 +0800

    third

--- hw3/file.h
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <inttypes.h>
-
-typedef unsigned char uchar;
-typedef uint32_t u32;
-
-int load_binaryFile(const char *filename, uchar *a, int max_size);
-void write_binaryFIle(const char *filename, uchar *a, int size);
