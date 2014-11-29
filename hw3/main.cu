745d409466007b55e94bb9f9ed9fb16dc566fc6a
WoodyYo <lturtsamuel@gmail.com>
2014-11-30 02:31:21 +0800

    third

--- hw3/main.cu
+++ hw3/main.cu
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <inttypes.h>
-#include "file.h"
 
 //
 #define PAGESIZE 32
@@ -12,11 +11,14 @@
 
 #define DATAFILE "./data.bin"
 #define OUTFILE "./snapshot.bin"
+
+#define MASK 32767
+#define TIME_MAX 4294967295
 typedef unsigned char uchar;
 typedef uint32_t u32;
 
 //
-__device__ __managed__ int PAGE_ENTRIES = 0;
+__device__ __managed__ int PAGE_ENTRIES = PHYSICAL_MEM_SIZE/PAGESIZE;
 //count the pagefault times
 __device__ __managed__ int PAGEFAULT = 0;
 
@@ -29,59 +31,129 @@ __device__ __managed__ uchar input[STORAGE_SIZE];
 
 //page table
 extern __shared__ u32 pt[];
+extern __shared__ u32 latest_time[];
 
-/******BLABLABLA~~****
-**********************/
+__shared__ u32 cur_time;
+
+/******BLABLABLA~~****/
+int load_binaryFile(const char *filename, uchar *a, int max_size) {
+	FILE *fp = fopen(filename, "rb");
+	int i = 0;
+	while(!feof(fp) && i < max_size) {
+		fread(a+i, sizeof(uchar), 1, fp);
+		i++;
+	}
+	return i;
+}
+
+void write_binaryFIle(const char *filename, uchar *a, int size) {
+	FILE *fp = fopen(filename, "wb+");
+	fwrite(a, sizeof(uchar), size, fp);
+}
+
+__device__ u32 lru() {
+	u32 min = TIME_MAX;
+	int victim_index = 0;
+	for(int i = 0; i < PAGE_ENTRIES; i++) {
+		if(latest_time[i] == 0) return i;
+		else {
+			if(latest_time[i] < min) {
+				min = latest_time[i];
+				victim_index = i;
+			}
+		}
+	}
+	return victim_index;
+}
+__device__ int find(u32 p) {
+	for(int i = 0; i < PAGE_ENTRIES; i++) {
+		u32 cur_p = (pt[i]>>15);
+		if(cur_p == p) {
+			return i;
+		}
+	}
+	return -1;
+}
+__device__ u32 paging(uchar *buffer, u32 p, u32 offset) {
+	if(cur_time < TIME_MAX) cur_time++;
+	int p_index = find(p);
+	if(p_index == -1) {  //page fault!!
+		u32 victim_index = lru();
+		u32 frame = pt[victim_index]&MASK;
+		u32 victim_p = (pt[victim_index] >> 15);
+		for(int i = 0; i < 4; i++) {
+			storage[victim_p+i] = buffer[frame+i];
+			buffer[frame+i] = storage[p];
+		}
+		pt[victim_index] = ((p<<15)&frame);
+		latest_time[victim_index] = cur_time;
+		return frame + offset;
+	}
+	else {
+		latest_time[p_index] = cur_time;
+		return (pt[p_index]&MASK) + offset;
+	}
+}
+__device__ void init_pageTable(int pt_entries) {
+	cur_time = 0;
+	for(int i = 0; i < pt_entries; i++) {
+		pt[i] = (i<<15); //若還沒做Gwrite就做Gread可能會得到無意義的值，但這是使用者的錯誤，not my business.
+		latest_time[i] = 0;
+	}
+}
+/*********************/
 
 __device__ uchar Gread(uchar *buffer, u32 addr) {
-	u32 frame_num = addr/PAGESIZE;
+	u32 p = addr/PAGESIZE;
 	u32 offset = addr%PAGESIZE;
 
-	/*addr = paging(buffer, frame_num, offset);*/
+	addr = paging(buffer, p, offset);
 	return buffer[addr];
 }
 
 __device__ void Gwrite(uchar *buffer, u32 addr, uchar value) {
-	u32 frame_num = addr/PAGESIZE;
+	u32 p = addr/PAGESIZE;
 	u32 offset = addr%PAGESIZE;
 
-	/*addr = paging(buffer, frame_num, offset);*/
+	addr = paging(buffer, p, offset);
 	buffer[addr] = value;
 }
 
 __device__ void snapshot(uchar *result, uchar *buffer, int offset, int input_size) {
+	int tmp = PAGEFAULT;
 	for(int i = 0; i < input_size; i++) result[i] = Gread(buffer, i + offset);
+	PAGEFAULT = tmp;
 }
 
 __global__ void mykernel(int input_size) {
+		PAGEFAULT = 128;
+
 	//take shared memory as physical memory
 	__shared__ uchar data[PHYSICAL_MEM_SIZE];
 	//get page table entries
 	int pt_entries = PHYSICAL_MEM_SIZE/PAGESIZE;
 
 	//B4 1st Gwrite or Gread
-	/*init_pageTable(pt_entries);*/
+	init_pageTable(pt_entries);
 
 	//####Gwrite/Gread code section start####
 	for(int i = 0; i < input_size; i++) Gwrite(data, i, input[i]);
 	for(int i = input_size-1; i >= input_size-10; i--) int value = Gread(data, i);
 
 	//the last line of Gwrite/Gread code section should be snapshot()
-	snapshot(result, data, 0, input_size);
+	/*snapshot(result, data, 0, input_size);*/
 	//####Gwrite/Gread code section end####
-
-	printf("pagefault times = %d\n", PAGEFAULT);
 }
 
 int main() {
 	int input_size = load_binaryFile(DATAFILE, input, STORAGE_SIZE);
 
-	printf("%d\n", input_size);
-	/*mykernel<<<1, 1, 16384>>>(input_size);*/
 	cudaSetDevice(3);
+	mykernel<<<1, 1, 16384>>>(input_size);
 	cudaDeviceSynchronize();
 	cudaDeviceReset();
 
+	printf("pagefault times = %d\n", PAGEFAULT);
 	write_binaryFIle(OUTFILE, result, input_size);
 
 	return 0;
